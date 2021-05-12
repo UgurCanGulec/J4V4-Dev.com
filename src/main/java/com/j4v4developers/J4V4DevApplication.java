a85a39a7a96d9e7c3ee13b97e345772238b6d96a
UgurCanGulec <gulecugurcan@gmail.com>
2021-05-12 16:54:25 +0300

    Docker postgresql connection and management

--- src/main/java/com/j4v4developers/J4V4DevApplication.java
+++ src/main/java/com/j4v4developers/J4V4DevApplication.java
@@ -3,9 +3,11 @@ package com.j4v4developers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories
 public class J4V4DevApplication {
 
     public static void main(String[] args) {
